efb26ab489111b26ed46dd11161be617c5e1de44
Habib <habibleton@gmail.com>
2021-05-17 12:06:36 +1000

    yes

---
+++ Asig4_Arm_Game/Assets/Resources/GlobalController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame  
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene(0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
